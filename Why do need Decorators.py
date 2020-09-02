efbbc387dea8b4b1d56af2a8bc361b10c1d4de41
Hoshiyar Singh <hoshiyarhs9438625@gmail.com>
2020-09-02 23:22:16 +0530

    Add files via upload

---
+++ Why do need Decorators.py
@@ -0,0 +1,19 @@
+from time important time
+def performance (fn):
+
+def wrapper(*args, **kawrgs):
+
+t1= time()
+result= fn(*args, **kawrgs)
+
+t2= time()
+print(f'took {t2-t1}')
+  return result
+  return wrapper
+
+@performance
+def long_time()
+  for i in range(100000000):
+     i*5
+
+long_time()
